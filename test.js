c3d32bbe78f4aed843354b28461b4789a3160f2a
Wilfredo <casas.farach@gmail.com>
2020-04-12 23:09:51 +0200

    flow for job creation complete

--- test.js
+++ test.js
@@ -5,15 +5,21 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto("https://www.jobdirecto.com/");
+  await page.goto("https://thawing-depths-06900.herokuapp.com/");
   const mainButton = "//*[@id='app']/div/div/div[4]/button[1]";
+
   await page.waitForXPath(mainButton);
+
   await page.$eval(".buttonBasic", (el) => el.click());
   await page.$x("input[@class='MuiInputBase-input']");
+  const randomNum = await Math.floor(Math.random() * 100).toString();
+
   // restname
   await page.waitForSelector('input[name="restName"]');
   await page.focus('input[name="restName"]');
-  await page.keyboard.type("El potrero");
+  await page.keyboard.type(randomNum);
+
+  //   await page.$eval('input[name="restName"]', (el) => (el.value = randomNum));
   // jobtype
   await page.focus('input[name="jobType"]');
   await page.keyboard.type("cocinero de todas las comidas");
@@ -42,11 +48,26 @@ const puppeteer = require("puppeteer");
   // extraInfo
   await page.focus('textarea[name="extraInfo"]');
   await page.keyboard.type("que no fume durante el trabajo");
-  // click submit form
+  // click listo button
   const [listo] = await page.$x("(//button)[2]");
   if (listo) listo.click();
+  // click publish button
+  await page.waitForSelector("h2");
+  const [publish] = await page.$x("(//button)[4]");
+  if (publish) publish.click();
+  // verify that the post was created
+  await page.waitForSelector("h6");
 
-  //   await page.$eval(".MuiButtonBase-root", (el) => el.click());
+  const lastPost = await page.$(".fFAkCT");
+  const postId = await page.evaluate(
+    (lastPost) => lastPost.textContent,
+    lastPost
+  );
 
-  //   await browser.close();
+  if (postId === randomNum) {
+    console.log("Job post was created successfully");
+    browser.close();
+  } else {
+    console.error("Looks like someone fucked up over here");
+  }
 })();
