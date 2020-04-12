378c739d5a3e4dd66a6e4e931a93366ec68f69c2
Wilfredo <casas.farach@gmail.com>
2020-04-12 19:37:16 +0200

    filling job form

--- test.js
+++ test.js
@@ -1,15 +1,52 @@
+"use strict";
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 300 });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
   await page.goto("https://www.jobdirecto.com/");
-  const mainButton =
-    "#app > div > div > div.sc-cHGsZl.brchLR > button.MuiButtonBase-root.MuiButton-root.MuiButton-text.sc-htoDjs.hsDosS.sc-kgAjT.cnEATJ.buttonBasic";
-  await page.waitForSelector(mainButton);
-  //*[@id="app"]/div/div/div[4]/button[1]
-  await page.click(mainButton);
+  const mainButton = "//*[@id='app']/div/div/div[4]/button[1]";
+  await page.waitForXPath(mainButton);
+  await page.$eval(".buttonBasic", (el) => el.click());
   await page.$x("input[@class='MuiInputBase-input']");
-  console.log("hungry?");
-  await browser.close();
+  // restname
+  await page.waitForSelector('input[name="restName"]');
+  await page.focus('input[name="restName"]');
+  await page.keyboard.type("El potrero");
+  // jobtype
+  await page.focus('input[name="jobType"]');
+  await page.keyboard.type("cocinero de todas las comidas");
+  // hourPay
+  await page.focus('input[name="hourPay"]');
+  await page.keyboard.type("13 la hora");
+  // typePay
+  await page.click('input[name="typePay"]');
+  // schedule
+  await page.focus('input[name="schedule"]');
+  await page.keyboard.type("todos los dias");
+  // address
+  await page.focus('input[name="address"]');
+  await page.keyboard.type("el dorado str 123, brooklyn 12413");
+  // area
+  await page.click("#areaInput");
+  await page.waitForSelector("ul");
+  let lis = await page.$$("li");
+  await lis[2].click();
+  //phone
+  await page.focus('input[name="phone"]');
+  await page.keyboard.type("123");
+  // contact
+  await page.focus('input[name="contact"]');
+  await page.keyboard.type("pedro");
+  // extraInfo
+  await page.focus('textarea[name="extraInfo"]');
+  await page.keyboard.type("que no fume durante el trabajo");
+  // click submit form
+  const [listo] = await page.$x("(//button)[2]");
+  if (listo) listo.click();
+
+  //   await page.$eval(".MuiButtonBase-root", (el) => el.click());
+
+  //   await browser.close();
 })();
