8617493609c4da46a3544f176b25b9f5cd49236b
Wilfredo <casas.farach@gmail.com>
2020-04-13 12:40:52 +0200

    positions matching in page and modal

--- seeJobs.js
+++ seeJobs.js
@@ -6,9 +6,29 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto("https://thawing-depths-06900.herokuapp.com/");
-
   await page.waitForSelector("h6");
-
   let jobPosts = await page.$$("h5");
-  console.log(jobPosts.length);
+  const thirdJobPost = jobPosts[2];
+  const positionInMain = await page.evaluate(
+    (thirdJobPost) => thirdJobPost.textContent,
+    thirdJobPost
+  );
+  console.log(positionInMain);
+  // click job post
+  await thirdJobPost.click();
+  await page.waitForXPath("//div[contains(@class, 'bUwXfy')]");
+  const modalTexts = await page.$x("//div[contains(@class, 'bUwXfy')]");
+  console.log(modalTexts.length);
+  let secondTextModal = modalTexts[1];
+  const positionInModal = await page.evaluate(
+    (secondTextModal) => secondTextModal.textContent,
+    secondTextModal
+  );
+  console.log(positionInMain === positionInModal);
+  if (positionInMain === positionInModal) {
+  } else {
+    console.error("Someone fucked up over here");
+  }
+
+  browser.close();
 })();
