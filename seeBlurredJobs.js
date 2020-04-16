9373daa6132eec6a0294264d61b0066333df2166
Wilfredo <casas.farach@gmail.com>
2020-04-16 12:35:48 +0200

    changed input filling types, adding flow for payment inputs

--- seeBlurredJobs.js
+++ seeBlurredJobs.js
@@ -2,7 +2,10 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: "/usr/bin/google-chrome",
+  });
   const context = browser.defaultBrowserContext();
   context.overridePermissions("https://www.facebook.com", ["notifications"]);
   const page = await browser.newPage();
@@ -26,19 +29,40 @@ const puppeteer = require("puppeteer");
   const loginButton = await page.waitForXPath(
     "//button[contains(@name, 'login')]"
   );
-  await page.waitForSelector("#email");
-  await page.focus("#email");
-  await page.keyboard.type("casas.farach@yahoo.com");
-  await page.focus("#pass");
-  await page.keyboard.type("789654123");
-  await loginButton.click();
+  const email = await page.waitForSelector("#email");
+  const pass = await page.waitForSelector("#pass");
 
-  const continueASButton = await page.waitForXPath(
-    "//button[contains(@name, '__CONFIRM__')]"
+  // await page.focus("#email");
+  await page.evaluate((text) => {
+    email.value = text;
+  }, "casas.farach@yahoo.com");
+
+  await page.evaluate((text) => {
+    pass.value = text;
+  }, "789654123");
+  await loginButton.click();
+  console.log("-1");
+
+  // const continueASButton = await page.waitForXPath(
+  //   "//button[contains(@name, '__CONFIRM__')]"
+  // );
+
+  // back in jobdirecto
+  await page.waitFor(2000);
+  await page.waitForSelector("form");
+  console.log("2");
+  const nameInput = await page.waitForXPath(
+    "//input[contains(@name, 'cardHolderName')]"
   );
 
-  await continueASButton.click();
+  await nameInput.focus();
 
-  // await page.type("#m_login_email", "asda");
-  // await page.type("#password", "789654123");
+  await page.keyboard.type("casas.farach@yahoo.com");
+
+  const emailInput = await page.waitForXPath(
+    "//input[contains(@name, 'cardHolderEmail')]"
+  );
+  const cardInput = await page.waitForXPath(
+    "//input[contains(@name, 'cardnumber')]"
+  );
 })();
