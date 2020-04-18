f5f15ccea645bc9b97dade9a33f934714899a0e7
Wilfredo <casas.farach@gmail.com>
2020-04-18 10:58:57 +0200

    including case where anotherCard happens

--- nopremiumflow.js
+++ nopremiumflow.js
@@ -35,50 +35,60 @@ const puppeteer = require("puppeteer");
   await page.evaluate((text) => {
     email.value = text;
   }, "nopremiumjd@gmail.com");
-  await page.waitFor(1000);
+  await page.waitFor(500);
   await page.evaluate((text) => {
     pass.value = text;
   }, "jobdirectoiscool");
-  await page.waitFor(1500);
+  await page.waitFor(500);
   await loginButton.click();
 
   const cookiesAlert = await page
     .waitForXPath(
       "//div[@class='login_form_container']//div[@id='error_box']",
       {
-        timeout: 10000,
+        timeout: 6000,
       }
     )
     .catch((e) => void e);
 
   if (cookiesAlert) {
+    const pass = await page.waitForSelector("#pass");
+    const loginButton = await page.waitForXPath(
+      "//button[contains(@name, 'login')]"
+    );
     console.log("cookiesAlert appeared, continue now");
     await page.evaluate((text) => {
       pass.value = text;
     }, "jobdirectoiscool");
     await page.waitFor(1500);
-    const loginButton = await page.waitForXPath(
-      "//button[contains(@name, 'login')]"
-    );
     await loginButton.click();
-    await page.waitFor(5000);
   }
 
-  console.log("lets continue and forget the past");
-  const [continueASButton] = await page.$x(
-    "//button[contains(@name, '__CONFIRM__')]"
-  );
+  console.log("Time for payment");
+
+  const continueASButton = await page
+    .waitForXPath("//button[contains(@name, '__CONFIRM__')]", {
+      timeout: 3000,
+    })
+    .catch((e) => void e);
 
   if (continueASButton) {
     console.log("continueAs button appeared");
     continueASButton.click();
   }
 
-  // if (usar otra tarjeta) {
-  //   change to form
-  // }
+  const anotherCard = await page
+    .waitForXPath("//span[contains(text(),'otra tarjeta')]", {
+      timeout: 3000,
+    })
+    .catch((e) => void e);
+
+  if (anotherCard) {
+    anotherCard.click();
+  }
 
   // back in jobdirecto
+
   await page.waitForSelector("form");
   await page.$eval(
     "input[name=cardHolderName]",
